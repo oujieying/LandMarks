e77a5238a245817789e2a32f6e5290366d8dd2a6
oujieying <ou278136922@gmail.com>
2020-03-17 16:35:51 +0800

    Initial Commit

---
+++ LandMarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LandMarks
+//
+//  Created by jieying ou on 17/3/2020.
+//  Copyright © 2020 jieying ou. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
